98970d9f82c83d8d2ed72310f3dfe321feb71b09
Notebook <damir-aldiyarovich@mail.ru>
2024-01-21 23:24:18 +0600

    glavnayaq

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +27,6 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
 }
+
